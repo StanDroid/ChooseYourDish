511f86eda2428839fa85e395adb4ebf4b0895075
stas1270 <stassafyanov@gmail.com>
2022-12-28 17:14:47 +0200

    revised gradle dependencies

--- app/src/main/java/com/cyd/core/BaseActivity.kt
+++ app/src/main/java/com/cyd/core/BaseActivity.kt
@@ -1,13 +1,5 @@
 package com.cyd.core
 
 import androidx.activity.ComponentActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 
-open class BaseActivity : ComponentActivity() {
-
-    protected inline fun <reified T : ViewModel> getViewModel(): T {
-        return ViewModelProvider(this).get(T::class.java)
-    }
-
-}
+open class BaseActivity : ComponentActivity()
