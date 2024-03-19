b124ecb5a9377b172cdf2a4bebdbeead96339c05
StanDroid <stassafyanov@gmail.com>
2024-03-19 23:01:21 +0100

    updated versions

--- app/src/main/java/com/cyd/core/navigation/route/RandomMealRoute.kt
+++ app/src/main/java/com/cyd/core/navigation/route/RandomMealRoute.kt
@@ -2,24 +2,24 @@ package com.cyd.core.navigation.route
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.cyd.R
 import com.cyd.core.navigation.NavScreen
 import com.cyd.feature.random_meal.RandomMealScreen
 import com.cyd.feature.random_meal.viewmodel.RandomMealUiState
 import com.cyd.feature.random_meal.viewmodel.RandomMealViewModel
 import com.cyd.ui.view.base.MealScaffold
 
-@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun RandomMealRoute(
     navController: NavHostController
 ) {
     val viewModel = hiltViewModel<RandomMealViewModel>()
     val state: RandomMealUiState by viewModel.uiState.collectAsStateWithLifecycle()
-    MealScaffold("Dish Of The Day") {
+    MealScaffold(stringResource(R.string.dish_of_the_day)) {
         RandomMealScreen(
             state,
             viewModel::onLoadNextRandomMealClick
