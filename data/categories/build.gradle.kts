cc4bba90bb9f55802f95a1fa0257b790b8f664c4
StanDroid <stassafyanov@gmail.com>
2024-07-02 13:56:21 +0200

    migrated to kts

---
+++ data/categories/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+apply(from = "${project.rootDir}/jacoco/jacoco.gradle")
+
+android {
+    namespace = "com.cyd.data.categories"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":data:network"))
+    implementation(project(":core:base"))
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
