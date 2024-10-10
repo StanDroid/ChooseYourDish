068ee121554b6e01620f5892b3a5d65e1d46a628
StanDroid <stassafyanov@gmail.com>
2024-10-10 20:33:23 +0200

    added search screen

--- app/src/main/java/com/cyd/core/navigation/route/SearchRoute.kt
+++ app/src/main/java/com/cyd/core/navigation/route/SearchRoute.kt
@@ -1,23 +1,17 @@
 package com.cyd.core.navigation.route
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.cyd.search.SearchScreen
 import com.cyd.search.SearchViewModel
+import com.cyd.ui.view.base.MealScaffold
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @ExperimentalMaterial3Api
@@ -26,48 +20,17 @@ fun SearchRoute(
     navController: NavHostController
 ) {
     val viewModel = hiltViewModel<SearchViewModel>()
-//    val state by remember { viewModel.uiState }
-    //Collecting states from ViewModel
-    val searchText by viewModel.searchText.collectAsState()
-    val resultedText by viewModel.resultedText.collectAsState()
-    val isSearching by viewModel.isSearching.collectAsState()
-    val countriesList by viewModel.countriesList.collectAsState()
+    val state by viewModel.searchViewModelState.collectAsState()
 
-
-
-    Scaffold(
-//        stringResource(R.string.ingredients),
-//        icon = Icons.Default.ArrowBack,
-//        onIconClick = { navController.navigateUp() }
-        topBar = {
-            SearchBar(
-                query = searchText,//text showed on SearchBar
-                onQueryChange = viewModel::onSearchTextChange, //update the value of searchText
-                onSearch = viewModel::onSearchTextChange, //the callback to be invoked when the input service triggers the ImeAction.Search action
-                active = isSearching, //whether the user is searching or not
-                onActiveChange = { viewModel.onToogleSearch() }, //the callback to be invoked when this search bar's active state is changed
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                LazyColumn {
-                    items(countriesList) { country ->
-                        Text(
-                            text = country,
-                            modifier = Modifier
-                                .padding(
-                                    start = 8.dp,
-                                    top = 4.dp,
-                                    end = 8.dp,
-                                    bottom = 4.dp
-                                )
-                                .clickable { viewModel.onCountryClick(country) },
-                        )
-                    }
-                }
-            }
-        }
-    ) {
-        Text(resultedText)
+    MealScaffold(
+        "Ingredients",
+        icon = Icons.AutoMirrored.Filled.ArrowBack,
+        onIconClick = { navController.navigateUp() }) {
+        SearchScreen(
+            state,
+            onSearchTextChange = viewModel::onSearchTextChange,
+            onToggleSearch = viewModel::onToogleSearch,
+            onCountryClick = viewModel::onCountryClick
+        )
     }
 }
--- feature/search/src/main/java/com/cyd/search/SearchScreen.kt
+++ feature/search/src/main/java/com/cyd/search/SearchScreen.kt
@@ -1,14 +1,49 @@
 package com.cyd.search
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SearchScreen() {
-    Box(Modifier.fillMaxSize()) {
-        Text(text = "Search")
+fun SearchScreen(
+    state: SearchViewModel.SearchViewModelState,
+    onSearchTextChange: (String) -> Unit,
+    onToggleSearch: () -> Unit,
+    onCountryClick: (String) -> Unit
+) {
+    Column(Modifier.fillMaxSize()) {
+        SearchBar(
+            query = state.searchText,//text showed on SearchBar
+            onQueryChange = onSearchTextChange::invoke, //update the value of searchText
+            onSearch = onSearchTextChange::invoke, //the callback to be invoked when the input service triggers the ImeAction.Search action
+            active = state.isSearching, //whether the user is searching or not
+            onActiveChange = { onToggleSearch.invoke() }, //the callback to be invoked when this search bar's active state is changed
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            LazyColumn {
+                items(state.countriesList) { country ->
+                    Text(
+                        text = country,
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .clickable { onCountryClick(country) },
+                    )
+                }
+            }
+        }
+        Text("Selected Country: ${state.searchText}")
     }
 }
