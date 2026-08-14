abaac6c5a8a509adbb43cfeaeea17be6431cc5b0
StanDroid <stassafyanov@gmail.com>
2026-08-14 18:01:14 +0200

    increased app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.cyd"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.11"
+        versionCode = 12
+        versionName = "0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
